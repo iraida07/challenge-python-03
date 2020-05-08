3f1e8f25772d7f86e0130a213746c6e4eb3abdca
iraida07 <iraidabarreto07@hotmail.com>
2020-05-07 19:37:22 -0500

    :sparkles: Desarrollar función run y darle solución al reto

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 # Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
+    # Start  coding here
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
 
+        secret_message = ''.join(re.findall('[a-z]', f.read()))
+        print (secret_message)
 
 if __name__ == '__main__':
     run()
